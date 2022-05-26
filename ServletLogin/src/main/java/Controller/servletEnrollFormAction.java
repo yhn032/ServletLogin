d93c2193f5b1fcff17858313e6ded2b868034cb5
KBK <yhn0485761@gmail.com>
2022-05-26 16:49:53 +0900

    [Kim] 서블릿 교통정리중

--- ServletLogin/src/main/java/Controller/servletEnrollFormAction.java
+++ ServletLogin/src/main/java/Controller/servletEnrollFormAction.java
@@ -10,26 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class servletMainAction
+ * Servlet implementation class servletEnrollAction
  */
-@WebServlet("/member/main.do")
-public class servletMainAction extends HttpServlet {
+
+//회원가입에 성공하면 테이블에 값을 insert
+@WebServlet("/member/insert_form.do")
+public class servletEnrollFormAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		
-		
 		//forward
-		String forward_page = "main.jsp";
+		String forward_page = "enroll_form.jsp";
 		RequestDispatcher disp = request.getRequestDispatcher(forward_page);
 		disp.forward(request, response);
-		
 	}
 
 }
--- ServletLogin/src/main/java/Controller/servletLoginAction.java
+++ ServletLogin/src/main/java/Controller/servletLoginAction.java
@@ -26,40 +26,29 @@ public class servletLoginAction extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//로그인한 정보를 login_form.jsp로부터 전달받는 서블릿. 데이터를 가공해서 로그인 성공 여부를 판단한다.
 		
-		String email = request.getParameter("email");
-		String pwd = request.getParameter("pwd");
-		
-		//처음 포워딩시(로그인 화면에 처음 들어온 경우)
-		if(email ==null || pwd == null) {//전달받은 parameter값이 없음
-			RequestDispatcher disp = request.getRequestDispatcher("login.jsp");
-			disp.forward(request, response);
-		}
-		
-		int result = ModelDao.getInstance().login(email, pwd);
-		System.out.println(result);
-		//~님 환영합니다에서 ~를 sessionid값을 사용하면 좋을 것같음
-		if(result == 1) {//로그인이 성공했다면
-			//로그인한 사용자의 아이디 정보로 정보 얻어오기
-			ModelVo vo = ModelDao.getInstance().selectFromemail(email);
-			String nickname = vo.getNickname();
-			HttpSession session = request.getSession();
-			session.setAttribute("sessionID", nickname); //세션값으로 로그인한 사용자의 닉네임 정보 기억
-			response.sendRedirect("main.do");
-			
-		}else { //로그인에 실패했다면
-			request.setAttribute("result", result);
-			RequestDispatcher disp = request.getRequestDispatcher("login.jsp");
-			disp.forward(request, response);
-			
-		}
-		
-		
-		
-		/*
-		 * //forward String forward_page = "login.jsp"; RequestDispatcher disp =
-		 * request.getRequestDispatcher(forward_page); disp.forward(request, response);
-		 */
+		  //파라미터 수신. 한글이 없으니까 수신인코딩안해도 될듯. (닉네임을 메인화면에 출력하고자 한다면 해줘야 함)
+		  String email = request.getParameter("email"); 
+		  String pwd = request.getParameter("pwd");
+		  
+		  //로그인 성공여부 파악. 1 : 성공, -1 : 둘 다 틀림, 0 : 비번만 틀림
+		  int result = ModelDao.getInstance().login(email, pwd);
+		  
+		  if(result== 1) {//로그인이 성공했다면 //로그인한 사용자의 이메일 정보로 정보 얻어오기 
+			  ModelVo vo = ModelDao.getInstance().selectFromemail(email); 
+			  
+			  HttpSession session = request.getSession();
+			  session.setAttribute("user", vo); //세션값으로 로그인한 사용자의 정보 기억
+			  response.sendRedirect("main.do");
+		  
+		  }else { //로그인에 실패했다면 로그인 실패 정보를 가지고 다시 로그인 폼으로 이동
+			  request.setAttribute("result", result);
+			  RequestDispatcher disp = request.getRequestDispatcher("login_form.jsp");
+			  disp.forward(request, response);
+		  
+		  }
+
 	}
 
 }
